b03951d0045eefbecc4ce402a943596f20020f6c
AusiasBM <ausiasbm@gmail.com>
2024-04-02 17:35:47 +0200

    feat: Funcionan en IOS y Android

---
+++ ios/ImageNotification/NotificationService.h
@@ -0,0 +1,12 @@
+//
+//  NotificationService.h
+//  ImageNotification
+//
+//  Created by Ausias BM on 2/4/24.
+//
+
+#import <UserNotifications/UserNotifications.h>
+
+@interface NotificationService : UNNotificationServiceExtension
+
+@end
